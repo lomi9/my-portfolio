9715ee2cf59c52d963d708840bdc4c67a377a95b
lomi9 <119067005+lomi9@users.noreply.github.com>
2024-01-11 00:59:59 +0100

    feat: update presentation text

--- src/Components/Presentation/Presentation.jsx
+++ src/Components/Presentation/Presentation.jsx
@@ -9,10 +9,12 @@ export default function Presentation() {
 
     const pictureRef = useRef(null);
     const titleRef = useRef(null);
-    const textRef = useRef(null);
+    const textRef1 = useRef(null);
+    const textRef2 = useRef(null);
+    const textRef3 = useRef(null);
 
     useEffect(() => {
-        if (pictureRef.current && titleRef.current && textRef.current) {
+        if (pictureRef.current && titleRef.current && textRef1.current && textRef2.current && textRef3.current) {
             const tl = gsap.timeline({
                 scrollTrigger: {
                     trigger: '.presentation',
@@ -28,9 +30,15 @@ export default function Presentation() {
               .fromTo(titleRef.current, 
                 { y: 50, opacity: 0 }, 
                 { y: 0, opacity: 1, duration: 1, ease: "power1.out" }) 
-              .fromTo(textRef.current, 
+              .fromTo(textRef1.current, 
                 { y: 50, opacity: 0 }, 
-                { y: 0, opacity: 1, duration: 1, ease: "power1.out" }); 
+                { y: 0, opacity: 1, duration: 0.8, ease: "power1.out" })
+              .fromTo(textRef2.current, 
+                { y: 50, opacity: 0 }, 
+                { y: 0, opacity: 1, duration: 0.8, ease: "power1.out" })
+              .fromTo(textRef3.current, 
+                { y: 50, opacity: 0 }, 
+                { y: 0, opacity: 1, duration: 0.8, ease: "power1.out" });
         }
 
 
@@ -48,9 +56,17 @@ export default function Presentation() {
             <div className="presentation__content">
                 <div className="presentation__content-description">
                     <h2 ref={titleRef} className="presentation__content-description-title">Á PROPOS DE MOI</h2>
-                    <p ref={textRef} className="presentation__content-description-text">
-                    Je suis Loriane, une passionnée de développement web actuellement en phase finale d'une année de formation de développeur web avec OpenClassrooms. Je recherche activement une opportunité de contrat d'apprentissage ou tout autre dispositif de formation pratique au sein d'une entreprise dynamique. Mon objectif est de renforcer mes compétences en tant que développeuse front-end ou full-stack, en contribuant avec créativité et efficacité aux projets sur lesquels je travaillerai. Si vous recherchez une développeuse motivée et prête à relever de nouveaux défis, n'hésitez pas à me contacter!
-                    </p>
+                    <div className="presentation__content-description-text">
+                        <p ref={textRef1} className="presentation__content-description-text-p">
+                        Je suis Loriane, une passionnée de développement web venant tout juste de terminer avec succès une année de formation de développeur web avec OpenClassrooms. Je recherche activement une opportunité de contrat d’apprentissage, stage, ou toute autre opportunité de formation au sein d’une entreprise dynamique.
+                        </p>
+                        <p ref={textRef2} className="presentation__content-description-text-p">
+                        Mon objectif : Renforcer mes compétences en tant que développeuse front-end ou full stack, et surtout continuer d’apprendre. En effet, j’aime passer des heures à créer des designs avec CSS, créer des composants avec React, ou dynamiser une page avec JavaScript. J’aime également le back-end, pour lequel j’aimerais également me former.
+                        </p>
+                        <p ref={textRef3} className="presentation__content-description-text-p">
+                        Sortant tout juste de formation, je ne passerai certainement pas tous les tests immédiatement. Cependant, étant quelqu’un d’autonome et très persévérante, je sais rechercher les solutions à mes problèmes afin de les résoudre. Si vous cherchez une personne motivée, et jamais lassée d’apprendre, n’hésitez pas à me contacter.
+                        </p>
+                    </div>
                 </div>
             </div>
 
