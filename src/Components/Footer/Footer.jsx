3d751fa567b1b43a0d8418031dcf67f1f2becb88
lomi9 <119067005+lomi9@users.noreply.github.com>
2024-01-11 17:53:25 +0100

    feat : add linkedin link in footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,4 +1,6 @@
 import logo from "../../assets/logo_transparent_beige.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer () {
 
@@ -6,6 +8,9 @@ export default function Footer () {
         <div className="footer">
             <div className="footer__content">
                 <img src={logo} alt="logo - Loriane MICHEL" className="footer__content-logo"/>
+                <div className="footer__content-link">
+                <a target="_blank" rel="noopener noreferrer" href="www.linkedin.com/in/loriane-michel-b791a6268" className="footer__content-link-a"><FontAwesomeIcon icon={faLinkedin} className="footer__content-link-a-linkedin"/></a>
+                </div>
                 <span className="footer__content-text">Dernière mise à jour : Janvier 2024</span>
             </div>
 
